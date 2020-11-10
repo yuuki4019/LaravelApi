b8a6eb9bd394952cef9f1aa7572d670b849bc320
y.mouri <y.mouri@rea-trip.com>
2020-11-11 08:54:52 +0900

    first commit

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['api']], function () {
+    //一覧表示
+    Route::get('vehicles','Api\VehicleController@index');
+    //指定した車両を取得
+    Route::get('vehicles/{id}','Api\VehicleController@show');
+    //新規登録
+    Route::post('vehicles','Api\VehicleController@store');
+    //アップデート
+    Route::put('vehicles/{id}','Api\VehicleController@update');
+});
